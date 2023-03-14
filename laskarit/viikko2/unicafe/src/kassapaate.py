3b11c938c10bd022adf13d842b9f3e16094ac2a5
Ville Saastamoinen <trickwide@gmail.com>
2023-03-14 13:46:28 +0200

    kassapaate.py refaktorointi ja testien lisääminen

--- laskarit/viikko2/unicafe/src/kassapaate.py
+++ laskarit/viikko2/unicafe/src/kassapaate.py
@@ -1,36 +1,45 @@
 class Kassapaate:
+    EDULLINEN = 240
+    MAUKAS = 400
     def __init__(self):
         self.kassassa_rahaa = 100000
         self.edulliset = 0
         self.maukkaat = 0
+        
+    def _kateismaksu(self, maksu, hinta):
+        if maksu >= hinta:
+            self.kassassa_rahaa += hinta
+            self._paivita_myydyt_lounaat(hinta)
+            return maksu - hinta
+        else:
+            return maksu
+          
+    def _paivita_myydyt_lounaat(self, hinta):
+        if hinta == self.EDULLINEN:
+            self.edulliset += 1
+        elif hinta == self.MAUKAS:
+            self.maukkaat += 1
 
     def syo_edullisesti_kateisella(self, maksu):
-        if maksu >= 240:
-            self.kassassa_rahaa = self.kassassa_rahaa + 240
-            self.edulliset += 1
-            return maksu - 240
-        else:
-            return maksu
+        return self._kateismaksu(maksu, self.EDULLINEN)
 
     def syo_maukkaasti_kateisella(self, maksu):
-        if maksu >= 400:
-            self.kassassa_rahaa = self.kassassa_rahaa + 400
-            self.maukkaat += 1
-            return maksu - 400
-        else:
-            return maksu
+        return self._kateismaksu(maksu, self.MAUKAS)
+          
+    def _on_tarpeeksi_rahaa_kortilla(self, kortti, summa):
+        return kortti.saldo >= summa
 
     def syo_edullisesti_kortilla(self, kortti):
-        if kortti.saldo >= 240:
-            kortti.ota_rahaa(240)
+        if self._on_tarpeeksi_rahaa_kortilla(kortti, self.EDULLINEN):
+            kortti.ota_rahaa(self.EDULLINEN)
             self.edulliset += 1
             return True
         else:
             return False
 
     def syo_maukkaasti_kortilla(self, kortti):
-        if kortti.saldo >= 400:
-            kortti.ota_rahaa(400)
+        if self._on_tarpeeksi_rahaa_kortilla(kortti, self.MAUKAS):
+            kortti.ota_rahaa(self.MAUKAS)
             self.maukkaat += 1
             return True
         else:
