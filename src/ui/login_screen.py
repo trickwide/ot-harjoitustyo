939e9ae0ba41ed8b6c515cc88b365222408b2b9a
Ville Saastamoinen <trickwide@gmail.com>
2023-04-03 11:43:01 +0300

    Added login functionality

--- src/ui/login_screen.py
+++ src/ui/login_screen.py
@@ -1,5 +1,7 @@
 from tkinter import ttk, constants
 import customtkinter
+import hashlib
+from database import create_connection, get_user
 
 class LoginScreen:
     """The LoginScreen class is the UI for the login screen."""
@@ -27,9 +29,28 @@ class LoginScreen:
         """Function to destroy the UI."""
         self._frame.destroy()
 
-    def validate_login():
+    def validate_login(self):
         """Function to validate the user's login credentials."""
-        pass
+        username = self._username_entry.get()
+        password = self._password_entry.get()
+        
+        if username and password:
+            # Hash the password
+            password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+            # Create a connection to the database
+            conn = create_connection("budget_tracker.db")
+            
+            # Get the user from the database
+            user = get_user(conn, username, password_hash)
+            
+            # If the user exists and password is correct, show the main window
+            if user:
+                self._show_main_window()
+                self.destroy()
+            else:
+                # SHOW ERROR MESSAGE, needs to be added
+                pass
     
     def _init_username_frame(self):
         username_label = ttk.Label(master=self._frame, text="Username")
@@ -54,7 +75,7 @@ class LoginScreen:
         self._init_password_frame()
         
         # Note to self, add command to the login button
-        login_button =  customtkinter.CTkButton(master=self._frame, corner_radius=20, text="Login")
+        login_button =  customtkinter.CTkButton(master=self._frame, corner_radius=20, text="Login", command=self.validate_login)
         
         self._frame.grid_columnconfigure(0, weight=1, minsize=400)
         
